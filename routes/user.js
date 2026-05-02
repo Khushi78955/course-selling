45d6bad39b31d7ec585bcb8a8558433db513c679
Khushi <khushithisside26@gmail.com>
2026-05-03 05:23:03 +0530

    password is hashed

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,31 @@
 // const Router = express.Router
 
 const { Router } = require("express");
+const { userModel } = require("../db");
+const bcrypt = require("bcrypt");
 
-const userRouter = Router()
 
-userRouter.post("/signup", function(req, res){
-    res.json({
-        message: "signup endpoint"
-    })
+const userRouter = Router();
+
+userRouter.post("/signup", async function(req, res){
+    const { email, password, firstName, lastName } = req.body;
+
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await userModel.create({
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName
+        })
+        res.json({
+            message: "Signup succeeded"
+        })
+    } catch(e){
+        res.status(500).json({
+            message: "User already exists or error"
+        })
+    }
 })
 
 userRouter.post("/signin", function(req, res){
