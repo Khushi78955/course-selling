6111888730e0354b3de457505119a9083d8e35bd
Khushi <khushithisside26@gmail.com>
2026-05-02 23:43:25 +0530

    db.js

---
+++ db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+mongoose.connect("")
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema = Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    createrId: ObjectId
+
+});
+
+const purchaseSchema = Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+})
+
+const userModel = mongoose.Model("user", userSchema);
+const adminModel = mongoose.Model("admin", adminSchema);
+const courseModel = mongoose.Model("course", courseSchema);
+const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+
+
+module.export = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
